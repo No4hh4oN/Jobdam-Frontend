717902293dbd5f83fa64e60f7db196d342f94ec5
heeyoung00 <ils09019@naver.com>
2025-07-31 19:10:14 +0900

    회원가입 수정 완료

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -2,11 +2,13 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 "use client";
 
-import { useState, ChangeEvent } from "react";
+import { useState, ChangeEvent, useEffect } from "react";
 import { useRouter } from 'next/navigation';
 import AxiosClient from "../AxiosClient";
 import BackNavigator from "@/components/backButton";
 import '../../styles/signup.css';
+import Image from 'next/image';
+
 
 
 interface FormState {
@@ -29,7 +31,7 @@ export default function Signup() {
     const [codeSent, setCodeSent] = useState(false); // 텍스트 : 인증번호 전송 -> 재전송 
     const [emailVerified, setEmailVerified] = useState<boolean>(false); // 확인여부에 따른 '다음' 버튼 활성화,비활성화
     const [emailVerifyFailed, setEmailVerifyFailed] = useState(false); // 인증번호 틀렸을때 메시지 나오게
-    
+    const [userIdCheck, setUserIdCheck] = useState<boolean | null>(null); //아이디 중복확인부분 , 초기값은 null
 
     const [form, setForm] = useState<FormState>({
         userEmail: "",
@@ -63,7 +65,7 @@ export default function Signup() {
                 },
             });
             console.log(res)
-            alert("인증번호가 이메일로 전송되었습니다.");
+            // alert("인증번호가 이메일로 전송되었습니다.");
             setCodeSent(true);
         }catch(error){
             console.error(error)
@@ -104,10 +106,12 @@ export default function Signup() {
                 },
             });
             console.log(res);
-            alert("사용 가능한 아이디입니다.");
+            setUserIdCheck(true);
+            // alert("사용 가능한 아이디입니다.");
         } catch (error) {
             console.error("중복 확인 실패", error);
-            alert("이미 사용 중인 아이디입니다.");
+            setUserIdCheck(false);
+            // alert("이미 사용 중인 아이디입니다.");
         }
     };
 
@@ -120,11 +124,11 @@ export default function Signup() {
     //회원가입하기 버튼 클릭시
     const handleRegister = async () => {
         if (!isPasswordValid(form.userPassword)) {
-            alert("비밀번호는 8~20자의 영문, 숫자, 특수문자를 포함해야 합니다.");
+            // alert("비밀번호는 8~20자의 영문, 숫자, 특수문자를 포함해야 합니다.");
             return;
         }
         if (form.userPassword !== form.passwordCheck) {
-            alert("비밀번호와 비밀번호 확인이 일치하지 않습니다.");
+            // alert("비밀번호와 비밀번호 확인이 일치하지 않습니다.");
             return;
         }
         try {
@@ -138,12 +142,12 @@ export default function Signup() {
                     authCode: authCodeNumber,
                 },
             });
-            alert("회원가입 성공");
+            // alert("회원가입 성공");
             setStep("complete");
             console.log(response)
         } catch (error) {
             console.error("회원가입 실패", error);
-            alert("회원가입 실패");
+            // alert("회원가입 실패");
         }
     };
 
@@ -151,11 +155,18 @@ export default function Signup() {
     const isAuthCodeEmpty = emailVerify.authCode.trim() === "";
     const isAuthCodeTyping = !isAuthCodeEmpty && !emailVerified;
 
+    const isFormVaild = 
+        userIdCheck===true && 
+        isPasswordValid(form.userPassword) && 
+        form.userPassword===form.passwordCheck
+
     return (
         <div className="Signup-Screen">
-            <BackNavigator />
+            {(step === "verify" || step === "form") && <BackNavigator />}
             <div className="SignUp-Box">
-                <div className="SignUp-Title">회원가입</div>
+                {(step === "verify" || step === "form") && (
+                    <div className="SignUp-Title">회원가입</div>
+                )}
 
             {step === 'verify' && (
                 <div className="SignUp-verify-Box">
@@ -200,28 +211,78 @@ export default function Signup() {
                     <div className="SignUp-next">
                         <button 
                             className={`SignUpButton ${emailVerified ? "nextbtn-purple" : "nextbtn-gray"}`} 
-                            onClick={() => setStep("form")} disabled={!emailVerified}>
-                            다음
+                            onClick={() => setStep("form")} 
+                            disabled={!emailVerified}
+                        > 
+                            다음 
                         </button>                        
                     </div>
                 </div>
             )}
             {step === "form" && (    
                 <div className="SignUp-input-Box">
-                    <input id="signId" type="text" name="userId" placeholder="아이디를 입력해주세요" value={form.userId}  onChange={handleChange} />
-                    <button onClick={handleCheck}>아이디 중복확인</button>
-                    <input id="signPass" type="password" name="userPassword" placeholder="비밀번호를 입력해주세요" value={form.userPassword} onChange={handleChange} />
-                    <input id="signcheckPass" type="password" name="passwordCheck" placeholder="비밀번호를 확인해주세요" value={form.passwordCheck || ""} onChange={handleChange} />
-                    <span>비밀번호는 8~20자의 영문, 숫자, 특수문자를 포함해야 합니다.</span>
+                    <div className="signup-id-check">
+                        <input 
+                            id="signId" 
+                            type="text" 
+                            name="userId" 
+                            placeholder="아이디를 입력해주세요" 
+                            value={form.userId}  
+                            onChange={handleChange}
+                            className={userIdCheck === false ? "error" : ""}
+                        />
+                        <button onClick={handleCheck}>중복확인</button>
+                    </div>
+                    {userIdCheck === null ? null :                     
+                        userIdCheck ? (
+                            <span className="userIdCheck-T">사용 가능한 아이디입니다.</span>
+                        ):(
+                            <span className="userIdCheck-F">중복된 아이디입니다.</span>
+                        )
+                    }
 
-                    <button className="SignUpButton" onClick={handleRegister}>회원가입하기</button>
+                    <input 
+                        id="signPass" 
+                        type="password" 
+                        name="userPassword" 
+                        placeholder="비밀번호를 입력해주세요" 
+                        value={form.userPassword} 
+                        onChange={handleChange} 
+                        className={!isPasswordValid(form.userPassword)&& form.userPassword ? "error" : ""}
+                    />
+                    {!isPasswordValid(form.userPassword) && form.userPassword && (
+                        <span className="userPassword-fail">비밀번호는 8~20자의 영문, 숫자, 특수문자를 포함해야 합니다.</span>
+                    )}
+                    
+                    <input 
+                        id="signcheckPass" 
+                        type="password" 
+                        name="passwordCheck" 
+                        placeholder="비밀번호를 확인해주세요" 
+                        value={form.passwordCheck || ""} 
+                        onChange={handleChange} 
+                        className={form.passwordCheck && form.userPassword !== form.passwordCheck ? "error" : ""}
+                    />
+                    {form.passwordCheck && form.userPassword !== form.passwordCheck &&(
+                        <span className="userPassword-fail">비밀번호가 일치하지 않습니다.</span>
+                    )}
+                    
+                    <button className={`SignUpButton ${isFormVaild ? "nextbtn-purple" : "nextbtn-gray"}`}  onClick={handleRegister} disabled={!isFormVaild}>회원가입하기</button>
                 </div>
             )}
             {step === "complete" && (
                 <div className="SignUp-complete-Box" >
-                    <h2>hi</h2>
+                    <Image
+                        src="/images/signupFinish.png"
+                        alt="완료체크"
+                        width={70}
+                        height={70}
+                        priority
+                    />
+                    <div>회원가입 완료!</div>
+                    <span>로그인 화면으로 돌아가 <br /> 로그인 해주세요.</span>
                     <button className="SignUpButton" onClick={() => router.push('/login')}>
-                        로그인하러가기
+                        로그인 화면으로
                     </button>
                 </div>
             )}            
